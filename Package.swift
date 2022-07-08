f778899155225da5d97b1d7622e7342920c4c49f
Philip Turner <philipturner.AR@gmail.com>
2022-07-08 18:05:53 -0400

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MetalExperiment1",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "MetalExperiment1",
+            targets: ["MetalExperiment1"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "MetalExperiment1",
+            dependencies: []),
+        .testTarget(
+            name: "MetalExperiment1Tests",
+            dependencies: ["MetalExperiment1"]),
+    ]
+)
---
+++ Sources/MetalExperiment1/MetalExperiment1.swift
@@ -0,0 +1,6 @@
+public struct MetalExperiment1 {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
